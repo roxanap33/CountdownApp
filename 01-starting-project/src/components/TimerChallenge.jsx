90f5f2b7867fa583db6e1ec448b6fa647ce86f89
roxanap33 <roxana.ciocian@principal33.com>
2024-03-14 16:27:49 +0200

    136: Add new reusable timer component

---
+++ 01-starting-project/src/components/TimerChallenge.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="">Timmer is running.../Timer inactive</p>
+    </section>
+  );
+}
