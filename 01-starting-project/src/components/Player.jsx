ec1c100c36e41a23413639ab102887a00ab37507
roxanap33 <roxana.ciocian@principal33.com>
2024-03-14 16:06:26 +0200

    133: Set and display player name using refs

--- 01-starting-project/src/components/Player.jsx
+++ 01-starting-project/src/components/Player.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const inputName = useRef();
 
-  function handleChange(event) {
-    setSubmitted(false);
-    setPlayerName(event.target.value);
-  }
+  const [playerName, setPlayerName] = useState(null);
 
   function handleClick() {
-    setSubmitted(true);
+    setPlayerName(inputName.current.value);
+    inputName.current.value = "";
   }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={playerName} />
+        <input ref={inputName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
